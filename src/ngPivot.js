37af2f2cbf8ba549d186afc16b825ebbd38bd818
Gabriel Muñoz <gmunoz@intellisoft.cl>
2015-02-04 11:32:23 -0300

    base

---
+++ src/ngPivot.js
@@ -0,0 +1,20 @@
+/**
+ * @ngdoc directive
+ * @name ngPivot.directive:ngPivot
+ *
+ * @description
+ * _Please update the description and restriction._
+ *
+ * @restrict A
+ * */
+
+
+angular.module('ngPivot')
+    .directive('ngPivot', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, elem, attr) {
+
+        }
+    };
+});
